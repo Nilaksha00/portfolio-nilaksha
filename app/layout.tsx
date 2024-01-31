a743f7e17a9737f4455d1f2873a34298228df701
Nilaksha00 <nilakshasperera@gmail.com>
2024-01-31 18:28:37 +0530

    header layout implemented partially

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Header from "@/components/header";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -16,7 +17,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-slate-950 text-slate-50`}>
-        {children}
+        <main className="grid grid-cols-12 gap-12 min-h-screen overflow-hidden">
+          <div className="col-span-4 col-start-2">
+            <div className="grid grid-rows-10 h-full">
+              <div className="row-span-8 row-start-2">
+                <Header />
+              </div>
+            </div>
+          </div>
+          <div className="col-span-6 col-start-6">
+            <div className="grid grid-rows-8 h-full">
+              <div className="row-span-6 row-start-2">{children}</div>
+            </div>
+          </div>
+        </main>
       </body>
     </html>
   );
---
+++ components/header.tsx
@@ -0,0 +1,133 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { motion } from "framer-motion";
+
+export default function Header() {
+  return (
+    <motion.div
+      className="flex flex-col justify-center space-y-12 h-full"
+      initial={{ x: -500, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+    >
+      {/* Name and position */}
+      <div>
+        <div className="m-0 p-0">
+          <p className="text-teal-400 xl:text-4xl lg:text-4xl md:text-3xl sm:text-3xl text-3xl font-bold tracking-wide p-0 m-0">
+            <span className="text-white">I'm Nilaksha</span> Perera
+          </p>
+        </div>
+
+        <div className=" text-white xl:text-xl lg:text-xl md:text-base sm:text-sm text-sm font-medium mt-2">
+          An Experienced Software Engineer
+        </div>
+        {/* Short intro */}
+        <div>
+          <div className=" text-white lg:text-base md:text-base sm:text-sm text-sm font-light tracking-wide mt-10">
+            I am a dedicated individual who loves to build digital excellence
+            with every line of code.
+          </div>
+        </div>
+      </div>
+
+      {/* Links for the content */}
+      <div className="flex flex-col gap-3">
+        {/* {content.map((item, index) => (
+          <div
+            className="flex flex-row items-center cursor-pointer w-fit"
+            key={index}
+            onClick={() => {
+              setActiveIndex(index);
+              console.log(activeIndex);
+            }}
+          >
+            {index === activeIndex ? (
+              <svg
+                width="70"
+                height="2"
+                viewBox="0 0 70 2"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="transition-all duration-1000 ease-in-out"
+              >
+                <path d="M0 0.797852L70 0.797852" stroke="#25FFCB" />
+              </svg>
+            ) : (
+              <svg
+                width="30"
+                height="2"
+                viewBox="0 0 30 2"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="transition-all duration-500 ease-linear"
+              >
+                <path d="M0 1.29785L30 1.29785" stroke="#7B7B7B" />
+              </svg>
+            )}
+
+            <div
+              className={`text-[10px] ml-4 font-medium tracking-widest uppercase ${
+                index === activeIndex ? "text-teal-400" : "text-neutral-500"
+              }`}
+            >
+              {item}
+            </div>
+          </div>
+        ))} */}
+      </div>
+
+      {/* CV and Social Links */}
+      <div>
+        <Link href="/nilaksha_resume.pdf" passHref>
+          <div className="w-fit h-fit px-4 py-1.5 bg-teal-400 bg-opacity-10 rounded-[9px] justify-center items-center gap-2 inline-flex hover:scale-95 transition-all duration-500 ease-in-out">
+            <div className="text-teal-400 text-[13px] mr-1 tracking-wide ">
+              See my resume
+            </div>
+
+            <Image src="/arrow.svg" width={20} height={11.3} alt="Arrow" />
+          </div>
+        </Link>
+        <div className="flex flex-row justify-start items-center gap-6 mt-6">
+          <Link href="https://www.linkedin.com/in/nilaksha00/" passHref>
+            <Image
+              src="/linkedin.svg"
+              width={18}
+              height={20}
+              alt="LinkedIn"
+              className="cursor-pointer hover:scale-90 transition-all duration-500 ease-in-out"
+            />
+          </Link>
+          <Link href="https://github.com/nilaksha00/" passHref>
+            <Image
+              src="/github.svg"
+              width={18}
+              height={20}
+              alt="GitHub"
+              className="cursor-pointer  hover:scale-90 transition-all duration-500 ease-in-out"
+            />
+          </Link>
+          <Link href="https://www.instagram.com/frostine.00/" passHref>
+            <Image
+              src="/instagram.svg"
+              width={18}
+              height={20}
+              alt="Instagram"
+              className="cursor-pointer  hover:scale-90 transition-all duration-500 ease-in-out"
+            />
+          </Link>
+          <Link href="https://medium.com/@nilaksha00" passHref>
+            <Image
+              src="/medium.svg"
+              width={18}
+              height={20}
+              alt="Medium"
+              className="cursor-pointer hover:scale-90 transition-all duration-500 ease-in-out"
+            />
+          </Link>
+        </div>
+      </div>
+    </motion.div>
+  );
+}
