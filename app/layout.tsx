1a1b5711c3714e8bc3a5e3ca26367258678a4c4c
Nilaksha00 <nilakshasperera@gmail.com>
2024-02-07 12:37:52 +0530

    header responsiveness implemented

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,13 +17,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} bg-gray-950 text-slate-50 xl:px-[6%] lg:px-[6%] md:px-[7%] sm:px-[8%] px-[10%]`}
+        className={`${inter.className} bg-gray-950 h-screen text-slate-50 xl:px-[6%] lg:px-[6%] md:px-[7%] sm:px-[8%] px-[10%] xl:py-[5%] py-[4%]`}
       >
         <main className="">
-          <div className="fixed top-0 w-1/3 h-screen flex items-center justify-center">
+          <div className="top-0 left-0 lg:w-1/3 xl:w-1/3 md:w-1/3 flex items-center justify-center w-full">
             <Header />
           </div>
-          {/* <div className="flex-1 flex items-center">{children}</div> */}
+          <div className="flex-1 flex items-center">{children}</div>
         </main>
       </body>
     </html>
